374d4c2c668e6068a9a5b2684cbd417549332a83
SiddhantPurohit225 <siddhantpurohit225@gmail.com>
2024-07-26 14:08:22 +0530

    added files of inner class

---
+++ src/InnerClass/Example1.java
@@ -0,0 +1,26 @@
+package InnerClass;
+
+
+class Outer {
+	static int outer_x = 100;
+
+	void test() {
+		//Inner inner = new Inner();
+		Inner.display();
+	}	
+
+	static class Inner {
+		static void display() {
+			System.out.println("display: outer_x = " + outer_x);
+		}
+	}
+}
+
+class Main {
+	public static void main(String args[]) {
+		Outer outer = new Outer();
+		outer.test();
+		Outer.Inner innerclass = new Outer.Inner();
+		innerclass.display();  // Correct way to call the static method
+	}
+}
