d6769653c58ef2e2d01d8e9d6673b78cbff6dbcb
SiddhantPurohit225 <siddhantpurohit225@gmail.com>
2024-07-26 14:13:01 +0530

    added more files for inner class

---
+++ src/InnerClass/Example2.java
@@ -0,0 +1,25 @@
+package InnerClass;
+
+class Outer {
+    int outer_x = 100;
+
+    void test() {
+        Inner inner = new Inner();
+        inner.display();
+    }
+
+    class Inner {
+        void display() {
+            System.out.println("display: outer_x = " + outer_x);
+        }
+    }
+}
+
+class Main {
+    public static void main(String args[]) {
+        Outer outer = new Outer();
+        outer.test();
+        Outer.Inner inner = outer.new Inner();  
+        inner.display();  
+    }
+}
